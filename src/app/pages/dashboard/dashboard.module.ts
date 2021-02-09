1f117ee1530dbbf88feffb0a68b3b16571bfe639
Diego Smagnotto <diego.smagnotto@gmail.com>
2021-02-09 11:16:27 -0300

    Subindo Module

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProdutosRoutingModule } from '../produtos/produtos-routing.module';
 import { MaterialUIModule } from 'src/app/material-ui.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard.component';
